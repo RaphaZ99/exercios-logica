245414d38f6dec1057fd6e0ade619a80b5323d0b
Raphael mota <raphael.mota14@hotmail.com>
2023-07-03 18:02:43 -0300

    Adicionado readme e exercicios dia 1

---
+++ EstudosLogica/Exercicios/ExercicioDia1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudosLogica.Exercicios
+{
+    public class ExercicioDia1
+    {
+        public void NumerosUmADez()
+        {
+            Console.WriteLine("Irá contar de 1 a 10");
+            var numberCount = 0;
+
+            while (numberCount <= 9)
+            {
+                numberCount++;
+                Console.WriteLine($"Número {numberCount}");
+            }
+
+        }
+        public void MediaTresNumeros(int numero1, int numero2, int numero3)
+        {
+            Console.WriteLine("Irá fazer o cálculo da média de 3 números");
+
+            Type type = typeof(ExercicioDia1);
+
+            MethodInfo methodInfo = type.GetMethod("MediaTresNumeros");
+
+            var media = (numero1 + numero2 + numero3) / methodInfo.GetParameters().Length;
+
+            Console.WriteLine($"A média é {media}");
+
+        }
+
+        public void IsParOuImpar(int numero)
+        {
+            Console.WriteLine("Irá verificar se o número é par ou impar");
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine($"O número é par {numero}");
+            }
+            else
+            {
+                Console.WriteLine($"O número é impar {numero}");
+            }
+
+        }
+
+        public void TabuadaNumero(int numero)
+        {
+            Console.WriteLine("Irá fazer a tabuada de um número");
+            var contagemTabuada = 1;
+
+            while (contagemTabuada <= 10)
+            {
+
+                Console.WriteLine($" {numero} * {contagemTabuada} = {numero * contagemTabuada}");
+                contagemTabuada++;
+
+            }
+
+        }
+
+        public void IsPrimo(int numero)
+        {
+            bool isPrimo = true;
+            var calculo = Math.Sqrt(numero);
+            if (numero < 2)
+            {
+
+                isPrimo = false;
+
+            }
+            else
+            {
+                for(int i = 2; i <= Math.Sqrt(numero); i++)
+                {
+                    if(numero % i == 0)
+                    {
+                        isPrimo = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrimo)
+            {
+                Console.WriteLine($"O número é primo {numero}");
+            }
+            else
+            {
+                Console.WriteLine($"O número não é primo {numero}");
+            }
+
+        }
+
+    }
+}
---
+++ EstudosLogica/Program.cs
@@ -0,0 +1,57 @@
+﻿using EstudosLogica.Exercicios;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Este programa irá mostrar 10 resoluções de lógica diárias.");
+        Console.WriteLine("Escolha o dia");
+        Console.WriteLine("1 - Dia 1");
+
+
+
+        var userInput = Console.ReadLine();
+
+        switch (userInput)
+        {
+            case "1":
+
+                var dia1 = new ExercicioDia1();
+
+                dia1.NumerosUmADez();
+
+                Console.WriteLine("Irá calcular a média de 3 números");
+
+                Console.WriteLine("Informe o Primeiro número :");
+                var numero1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Informe o Segundo número :");
+                var numero2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Informe o Terceiro número :");
+                var numero3 = Convert.ToInt32(Console.ReadLine());
+
+                dia1.MediaTresNumeros(numero1, numero2, numero3);
+
+                Console.WriteLine("Informe um número para verificar se ele é par ou impar :");
+                var numeroImparOuPar = Convert.ToInt32(Console.ReadLine());
+                dia1.IsParOuImpar(numeroImparOuPar);
+
+                Console.WriteLine("Informe um número para ser feito a tabuada do mesmo :");
+                var numeroTabuada = Convert.ToInt32(Console.ReadLine());
+                dia1.TabuadaNumero(numeroTabuada);
+
+                Console.WriteLine("Informe um número para verificar se o mesmo é primo");
+                var numeroPrimo = Convert.ToInt32(Console.ReadLine());
+                dia1.IsPrimo(numeroPrimo);
+                break;
+            default:
+                break;
+                Console.WriteLine("Nenhum dia escolhido");
+                break;
+
+        }
+
+    }
+
+}
